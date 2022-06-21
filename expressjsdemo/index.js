df7c8415a340bf540092f5c93a0a0055d157e631
shruthi093 <jayasruthi.l@gmail.com>
2022-06-21 19:42:48 +0530

    regex

--- expressjsdemo/index.js
+++ expressjsdemo/index.js
@@ -4,5 +4,4 @@ const getRoutes=require("./api/getRoutes")
 const postRoutes=require("./api/postRoutes")
 server.use("/user",getRoutes)
 server.use(postRoutes)
-
 server.listen(3001,()=>console.log("server started"))
---
+++ expressjsdemo/regexEg.js
@@ -0,0 +1,37 @@
+const express=require("express")
+const server=express()
+
+server.get(/\/*get$/,(req,res)=>{
+    res.send("dummy")
+})
+
+server.get(/\/[a-z]+car$/,(req,res)=>{
+    res.send("car route")
+})
+
+server.get(/\/[a-z0-9]{4,7}cake/,(req,res)=>{
+    res.send("cakes route")
+})
+
+server.get(/\/user|users|member/,(req,res)=>{
+    res.send("user details received")
+})
+
+/*server.get(/\/[a-zA-Z0-9$.]+@[a-zA-Z_]+.[a-z]{2,3}$/,(req,res)=>{
+    res.send("email accepted")
+})*/
+
+server.get("/route/:email([a-zA-Z0-9$.]+@[a-zA-Z_]+.[a-z]{2,3}$)/:username([a-z]{4,10})",(req,res)=>{
+    res.send(`recevied ${req.params.email}`)
+    
+})
+
+
+
+
+
+server.all("*",(req,res)=>{
+    res.send("page not found")
+})
+
+server.listen(3001,()=>console.log("server started"))
