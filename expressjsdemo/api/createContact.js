bbe7907a73eb0ce3963ebbba414093dbfdfa9cf9
shruthi093 <jayasruthi.l@gmail.com>
2022-06-30 19:36:31 +0530

    CRUD

--- expressjsdemo/api/createContact.js
+++ expressjsdemo/api/createContact.js
@@ -5,10 +5,10 @@ router.post("/createuser",(req,res)=>{
     const data=req.body
     console.log(data)
     contactsModel.create({
+        userid:data.userid,
         fname:data.fname,
-        phone:data.phone,
         email:data.email,
-        backlogs:data.backlogs 
+        role:data.role
     }).then((success)=>res.send("created successfully")).catch((err)=>{
         console.log(err)
         res.send("insert failed")
@@ -17,8 +17,19 @@ router.post("/createuser",(req,res)=>{
 router.get("/getcontactdetails",async (req,res)=>{
  /* contactsModel.find({}).then((success)=>res.send(success)).catch((err)=>console.log(err))*/
  const result=await contactsModel.find({})
- res.send(result)
+ res.send(result) 
+})
 
-  
+router.get("/getuser/:userid",async (req,res)=>{
+    let reqid=req.params.userid
+try{
+    const result=await contactsModel.findOne({userid:reqid})
+    res.send(result)
+}
+catch(err){
+    console.log(err)
+    res.send("error in fetch")
+}
+
 })
 module.exports=router
--- expressjsdemo/models/mongooseeg.js
+++ expressjsdemo/models/mongooseeg.js
@@ -2,20 +2,77 @@ const mongoose=require("mongoose")
 mongoose.connect("mongodb://localhost:27017/contactsdb").then((res)=>console.log("db is connected")).catch((err)=>console.log("error in connection"))
 const schema=mongoose.Schema
 const contactsSchema=new schema({
-    fname:String,
-    phone:Number,
-   /* email:{
-        type:String,
+    userid:{
+        type:Number,
+        unique:true,
         required:true
-    }*/
-    email:String,
-    backlogs:Boolean,
-  /*  address:{
+    },
+    fname:{
         type:String,
-        default:"IND"
-    }*/
+        required:true 
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true 
+    },
+    role:String
 })
+
+
+
 const contactsModel=mongoose.model("contact",contactsSchema)
+
+/*const document1=new contactsModel({
+    userid:0,
+    fname:"peter",
+    email:"peter@gmail.com",
+    role:"admin"
+})
+document1.save().then((res)=>console.log("inserted record successfully")).catch((err)=>console.log("error in create"))
+
+const document2=new contactsModel({
+    userid:1,
+    fname:"john",
+    phone:283238,
+    email:"john@gmail.com"
+})
+document2.save().then((res)=>console.log("inserted record successfully")).catch((err)=>console.log("error in create"))
+
+*/
+//delete
+
+/*contactsModel.findByIdAndDelete("62bda337b220e79498d6b9d5").then((res)=>console.log(res)).catch((err)=>console.log(err))
+
+contactsModel.deleteOne({userid:2}).then((res)=>console.log(res)).catch((err)=>console.log(err))
+
+contactsModel.deleteMany({userid:{$gte:0,$lte:2}}).then((res)=>console.log(res)).catch((err)=>console.log(err))*/
+
+
+
+//read operation
+
+/*contactsModel.find({userid:{$gte:0,$lte:2}},{email:1,userid:1}).then((response)=>console.log(response)).catch((err)=>console.log(err))
+
+contactsModel.find({userid:2,email:"helen@gmail.com"},{email:1,userid:1}).then((response)=>console.log(response)).catch((err)=>console.log(err))
+
+
+contactsModel.findById("62bda337b220e79498d6b9d6").then((response)=>console.log(response)).catch((err)=>console.log(err))*/
+
+// update
+
+/*contactsModel.findByIdAndUpdate("62bda337b220e79498d6b9d6",{fname:"helen mary",email:"helenmary@gmail.com"}).then((response)=>console.log(response)).catch((err)=>console.log(err))
+
+contactsModel.updateOne({userid:1},{fname:"johan"}).then((response)=>console.log(response)).catch((err)=>console.log(err))
+
+contactsModel.updateMany({userid:{$gte:0,$lte:2}},{designation:"trainers"}).then((response)=>console.log(response)).catch((err)=>console.log(err))*/
+
+
+
+
+
+
+
 
 module.exports=contactsModel
 /*const document1=new contactsModel({
