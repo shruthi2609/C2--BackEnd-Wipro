8eea1dde56bff3ea4ccb8952b7fc9650b0e770e3
shruthi093 <jayasruthi.l@gmail.com>
2022-07-05 19:48:29 +0530

    swagger documentation

--- sinontesting/index.js
+++ sinontesting/index.js
@@ -2,6 +2,32 @@ const express=require("express")
 const server=express()
 const bp=require("body-parser")
 server.use(bp.json())
+const SwaggerJsDoc=require("swagger-jsdoc")
+const SwaggerUi=require("swagger-ui-express")
+const options={
+    definition:{
+        openapi:"3.0.0",
+        info:{
+            title:"Express API with swagger",
+            version:"1.0.0",
+            description:"simple CRUD with swagger",
+            license:{
+                name:"MIT"
+            }
+        },
+        servers:[
+            {
+                url:"http://localhost:3001"
+            }
+        ]
+    },
+    apis:["./routes/ContactManagerRoutes.js"]
+}
+const specs=SwaggerJsDoc(options)
+server.use("/api-doc",
+SwaggerUi.serve,
+SwaggerUi.setup(specs,{explorer:true}))
+
 const mongoose=require("mongoose")
 const ContactManagerRoute=require("./routes/ContactManagerRoutes")
 mongoose.connect("mongodb://localhost:27017/c2sinon").then((res)=>console.log("connected to db")).catch((err)=>console.log(err))
--- sinontesting/routes/ContactManagerRoutes.js
+++ sinontesting/routes/ContactManagerRoutes.js
@@ -1,3 +1,85 @@
+/**
+*  @swagger
+*   components:
+*     schemas:
+*       Contact:
+*         type: object
+*         required:
+*           - fname
+*           - email
+*         properties:
+*           fname:
+*             type: string
+*             description: fname of the user
+*           email:
+*             type: string
+*             description: email of the user
+*           phone:
+*             type: string
+*             description: mobile number
+*         example:
+*            fname: Cary
+*            email: carry@gmail.com 
+*            phone: 293892382
+*/
+/**
+* @swagger
+*paths:
+*  "/create":
+*    post:
+*      summary: add a new user
+*      requestBody:
+*        description: required a desc
+*        required: true
+*        content:
+*          application/json:
+*            schema:
+*              "$ref": "#/components/schemas/Contact"
+*      responses:
+*        '201':
+*          description: Created user
+*        '404':
+*          description: not able to create
+*/
+/**
+* @swagger
+*paths:
+*  "/fectchall":
+*    get:
+*      summary: get all user
+*      responses:
+*        '200':
+*          description: fetched user
+*          content:
+*            application/json: 
+*              schema:
+*                "$ref": "#/components/schemas/Contact"
+*        '404':
+*          description: cant fetch
+*/
+/**
+* @swagger
+*   "/fetchuser/{userid}":
+*     get:
+*       summary: Gets a book by id
+*       tags: [Contacts]
+*       parameters:
+*         - in: path
+*           name: id
+*           schema:
+*             type: integer
+*           required: true
+*           description: The user id
+*       responses:
+*         "200":
+*           description: The list of user.
+*           content:
+*             application/json:
+*               schema:
+*                 $ref: '#/components/schemas/Contact'
+*         "404":
+*           description: user not found.
+*/
 const express=require("express")
 const router=express.Router()
 // const Router require("express").Router
@@ -6,7 +88,7 @@ const Controllers=require("../controllers/ContactManagerController")
 
 router.post("/create",Controllers.createUser)
 
-router.post("/createdata",Controllers.createData)
+
 
 router.get("/fetchuser/:userid",Controllers.fetchUserDetails)
 
