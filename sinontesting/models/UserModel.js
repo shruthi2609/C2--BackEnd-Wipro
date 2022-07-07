301723ed18eb48f7b7c3000827fd4966bcc8927f
shruthi093 <jayasruthi.l@gmail.com>
2022-07-07 19:33:30 +0530

    bcrypt compare and validations

--- sinontesting/models/UserModel.js
+++ sinontesting/models/UserModel.js
@@ -1,15 +1,24 @@
 const mongoose=require("mongoose")
 const Schema=mongoose.Schema
 const UserSchema=new Schema({
-    
     email:{
         type:String,
-        required:true,
-        unique:true 
+        required:[true,"enter email ! email is mandatory"],
+        unique:true
     },
     password:{
         type:String,
         required:true
+    },
+    phone:{
+        type:Number,
+    },
+    role:{
+        type:String,
+        enum:{
+            values:['admin','trainer','trainee'],
+            message:'{VALUE} is not a valid role'
+        }
     }
 })
 const UserModel=mongoose.model("UserModel",UserSchema)
--- sinontesting/routes/AuthenticationRoutes.js
+++ sinontesting/routes/AuthenticationRoutes.js
@@ -3,22 +3,52 @@ const router=express.Router()
 const UserModel=require("../models/UserModel")
 const bcrypt=require("bcrypt")
 router.post("/signup",async(req,res)=>{
+    try{
     const data=req.body
+    const user=await UserModel.findOne({email:data.email})
+    if(user){
+        res.status(404).send({msg:"email already exits",status:false})
+    }
    /* const salt=await bcrypt.genSalt(5)
     console.log("salt",salt)
     const hasedpassword=await bcrypt.hash(data.password,salt)
     console.log("hash",hasedpassword)*/
+    else{
     const hasedpassword=await bcrypt.hash(data.password,5)
     console.log("hash",hasedpassword)  
-try{
-    const result=UserModel.create({
+
+    const result=await UserModel.create({
         email:data.email,
-        password:hasedpassword
+        password:hasedpassword,
+        phone:data.phone,
+        role:data.role
     })
-    res.status(201).send("signup successfull")
+    res.status(201).send({msg:"signup successfull",status:true})
 }
+    }
 catch(err){
-    res.status(404).send(err)
+    console.log(err)
+    res.status(404).send({msg:"unknown error occured",status:false,err:err})
 }
+})
+router.post("/signin",async (req,res)=>{
+    const data=req.body
+    try{
+        const user=await UserModel.findOne({email:data.email})
+       if(user){
+       const comparison=await bcrypt.compare(data.password,user.password)
+       if(comparison){
+        res.status(200).send({msg:"login successfull",status:true})
+       }
+       else{
+        res.status(404).send({msg:"login is notsuccessfull,check your password",status:false})
+       }}
+       else{
+        res.status(404).send({msg:"login is notsuccessfull,email does not exists",status:false})
+       }
+    }
+    catch(err){
+        res.status(404).send(err)
+    }
 })
 module.exports=router
